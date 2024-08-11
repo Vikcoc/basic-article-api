4038f82894c53e925ad50e2f6b20b0cfa64ff963
Victor Cociobanu <vikcoc@yahoo.com>
2024-08-11 18:18:20 +0300

    Authentication and Authorization

--- basic-article-api/ArticleApplicationAuth/ArticleApplicationAuthConstants.cs
+++ basic-article-api/ArticleApplicationAuth/ArticleApplicationAuthConstants.cs
@@ -3,7 +3,7 @@
     public static class ArticleApplicationAuthConstants
     {
         public const string EmailParam = "email";
-        public const string PermissionSet = "roles";
+        public const string PermissionSet = "permissions"; // note to self, do not use roles as the property
         public const string PermissionSetReader = "reader";
         public const string PermissionSetAdmin = "admin";
     }
--- basic-article-api/Program.cs
+++ basic-article-api/Program.cs
@@ -1,4 +1,5 @@
 using basic_article_api.ArticleApplicationAuth;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,25 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<HttpClient>();
 builder.Services.AddAuthServices();
+
+builder.Services.AddAuthentication().AddJwtBearer(opt =>
+{
+    var key = builder.Configuration.GetSection("SigningCredentials:Auth:JWK").Get<JsonWebKey>();
+    var keySet = new JsonWebKeySet();
+    keySet.Keys.Add(key);
+
+    var parameters = new TokenValidationParameters()
+    {
+        ValidIssuer = builder.Configuration["SigningCredentials:Auth:Issuer"],
+        ValidAudience = builder.Configuration["SigningCredentials:Auth:Audience"],
+        IssuerSigningKeys = keySet.Keys,
+        RequireExpirationTime = true,
+        ClockSkew = TimeSpan.Zero
+    };
+
+    opt.TokenValidationParameters = parameters;
+});
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -22,11 +42,13 @@ if (app.Environment.IsDevelopment())
     app.AddDevAuth();
 }
 
+app.UseAuthorization();
+
 app.AddAuthRoutes();
 
 app.MapGet("/hello", () =>
 {
     return TypedResults.Ok("Hello World!");
-});
+}).RequireAuthorization(p => p.RequireClaim(ArticleApplicationAuthConstants.PermissionSet, ArticleApplicationAuthConstants.PermissionSetReader));
 
 app.Run();
